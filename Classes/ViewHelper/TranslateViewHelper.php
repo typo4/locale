e317f5cd6d90db2b3e848aea906794a1ecd82996
Roman Eckert <roman.eckert@inblau.de>
2020-10-21 13:31:20 +0200

    catch exceptions

--- Classes/ViewHelper/TranslateViewHelper.php
+++ Classes/ViewHelper/TranslateViewHelper.php
@@ -2,13 +2,18 @@
 
 namespace Typo4\Locale\ViewHelper;
 
+use Exception;
 use Typo4\Locale\Utility\LocaleLangUtility;
 use TYPO3\CMS\Core\Core\Environment;
 
 class TranslateViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\TranslateViewHelper {
     protected static function translate($id, $extensionName, $arguments, $languageKey, $alternativeLanguageKeys) {
         if (Environment::getContext()->isDevelopment()) {
-            LocaleLangUtility::update($id, $extensionName);
+            try {
+                LocaleLangUtility::update($id, $extensionName);
+            } catch(Exception $e) {
+            }
+
         }
 
         return parent::translate($id, $extensionName, $arguments, $languageKey, $alternativeLanguageKeys);
