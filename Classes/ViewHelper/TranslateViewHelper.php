f08b22fdb74f7dfbe2d5e5d25716ae9c681a4e51
Roman Eckert <roman.eckert@inblau.de>
2020-10-21 13:39:46 +0200

    check if id and extkey set

--- Classes/ViewHelper/TranslateViewHelper.php
+++ Classes/ViewHelper/TranslateViewHelper.php
@@ -2,18 +2,13 @@
 
 namespace Typo4\Locale\ViewHelper;
 
-use Exception;
 use Typo4\Locale\Utility\LocaleLangUtility;
 use TYPO3\CMS\Core\Core\Environment;
 
 class TranslateViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\TranslateViewHelper {
     protected static function translate($id, $extensionName, $arguments, $languageKey, $alternativeLanguageKeys) {
-        if (Environment::getContext()->isDevelopment()) {
-            try {
-                LocaleLangUtility::update($id, $extensionName);
-            } catch(Exception $e) {
-            }
-
+        if (is_string($id) && is_string($extensionName) && !empty($id) && !empty($extensionName) && Environment::getContext()->isDevelopment()) {
+            LocaleLangUtility::update($id, $extensionName);
         }
 
         return parent::translate($id, $extensionName, $arguments, $languageKey, $alternativeLanguageKeys);
